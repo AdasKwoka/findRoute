863686aafaa93d529c3d30943be5203d98faf799
Adam Kwoka <adam.kwoka99@gmail.com>
2021-11-23 23:17:24 +0100

    Add fetching data and implement algorythm to search route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import './styles/App.css';
+import { useState } from 'react';
+
+import DestinationPanel from './components/DestinationPanel';
 import Form from './components/Form';
 
+import './styles/App.css';
+
 function App() {
+  const [destination, setDestination] = useState("");
+
   return (
-    <>
-      <Form />
-    </>
+    <div className="wrapper">
+      <Form setDestination={setDestination}/>
+      <DestinationPanel destination={destination}/>
+    </div>
   );
 }
 
